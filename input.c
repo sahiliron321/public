15a9fcdec098885717583bd216506cf0fec1d349
sahiliron321 <149561616+sahiliron321@users.noreply.github.com>
2025-09-23 00:35:04 +0530

    Add files via upload

---
+++ input.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    float a;
+    float b;
+    float z=a*b;
+    scanf("enter length of rect%f",&a);
+    scanf("enter length of rect%f",&b);
+    printf("area of rect%f",z);
+    
+    
+    return 0;
+
+}
